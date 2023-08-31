364abb4a3d56aca7dc1b3a184c1cb84932db401a
vaka21fc <vaka21fc@student.ju.se>
2023-08-31 16:05:47 +0200

    Created a set of half-circles, and a set of ellipses

--- experiments/lab1.js
+++ experiments/lab1.js
@@ -3,20 +3,46 @@
 function setup() {
     createCanvas(500,500);
 }
-// Might delete draw function later, depending on whether I need it or not//
-function draw() {
-    background(254, 251, 243);
 
-// Basic Shapes//
-noStroke();
-fill(255,179,186);
-rect(200,0,100,100);
-fill(255,223,186);
-rect(200,100,100,100);
-fill(255,255,186);
-rect(200,200,100,100);
-fill(186,255,201);
-rect(200,300,100,100);
-fill(186,225,255);
-rect(200,400,100,100);
+function draw() {
+    background(246, 239, 214);
+
+// Color Palette//
+    let colors = [
+        color(178, 93, 166),
+        color(102, 136, 195),
+        color(72, 165, 106),
+        color(234, 175, 65),
+        color(206, 74, 74)
+      ];
+
+let numCircles = 5;
+let spacing = 100;
+
+// Half-circles//
+for (let i = 0; i < numCircles; i++) {
+  let centerX = 50;
+  let centerY = 50+i * spacing;
+  let radius = 45;
+  
+  let startAngle = HALF_PI;
+  let endAngle = -HALF_PI;
+  
+  let fillColor = colors[i];
+  
+  fill(fillColor);
+  arc(centerX, centerY, radius * 2, radius * 2, startAngle, endAngle, PIE);
+}
+
+//Full Circles
+for (let i = 0; i < numCircles; i++) {
+    let centerX = 100;
+    let centerY = 50+i * spacing;
+    let width = 90;
+ 
+    let fillColor = colors[i];
+    
+    fill(fillColor);
+    ellipse(centerX,centerY,90,90);
+  }
 }
