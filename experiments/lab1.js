ac3113b88e5eb2b15a9f5f1c3e268e12470c1512
vaka21fc <vaka21fc@student.ju.se>
2023-08-31 14:20:17 +0200

    Added a rainbow

--- experiments/lab1.js
+++ experiments/lab1.js
@@ -1,7 +1,22 @@
+//Setup Canvas//
+
 function setup() {
     createCanvas(500,500);
 }
-
+// Might delete draw function later, depending on whether I need it or not//
 function draw() {
     background(254, 251, 243);
+
+// Basic Shapes//
+noStroke();
+fill(255,179,186);
+rect(200,0,100,100);
+fill(255,223,186);
+rect(200,100,100,100);
+fill(255,255,186);
+rect(200,200,100,100);
+fill(186,255,201);
+rect(200,300,100,100);
+fill(186,225,255);
+rect(200,400,100,100);
 }
