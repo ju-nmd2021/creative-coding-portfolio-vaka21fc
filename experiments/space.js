6ee9e432b0cdf730b40096448a553a7483c5eb47
vaka21fc <vaka21fc@student.ju.se>
2023-09-07 11:44:19 +0200

    began with my lab

---
+++ experiments/space.js
@@ -0,0 +1,21 @@
+//Setup Canvas//
+
+function setup() {
+  createCanvas(500, 500);
+  noStroke();
+}
+
+function draw() {
+  background(5, 2, 30);
+}
+
+
+function stars ();{
+let colors = [
+    color(70, 30, 82),
+    color(221, 81, 127),
+    color(230, 142, 54),
+    color(85, 109, 200),
+    color(121, 152, 238),
+  ];
+}
