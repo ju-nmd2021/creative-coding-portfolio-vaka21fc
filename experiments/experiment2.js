e3d22fa2cf8e83b436551d3f00c6d708e9e4e6d9
vaka21fc <vaka21fc@student.ju.se>
2023-09-09 00:29:44 +0200

    Played around with feature that makes the ball change color whenever it hits a wall. Also cleaned up my file a bit.

--- experiments/experiment2.js
+++ experiments/experiment2.js
@@ -1,26 +1,45 @@
+//Defining my variables
 let x = 250;
 let y = 250;
 let speedX = 4;
 let speedY = 5;
+let ballColor;
+//
 
+//Defining the color palette for my ball//
+let colors = [
+  color(112, 214, 255),
+  color(255, 112, 166),
+  color(255, 151, 112),
+  color(255, 214, 112),
+  color(233, 255, 112),
+];
+//
+
+//Canvas Setup//
 function setup() {
-    createCanvas(500, 500);
-    noStroke();
-  }
-  
-  function draw() {
-    background(210);
-    ellipse(x,y,70);
+  createCanvas(500, 500);
+  ballColor = random(colors);
+}
+//
 
-    // Check for the walls
-    if (x > width || x < 0) {
-        speedX *= -1;
-      }
-      if (y > height || y < 0) {
-        speedY *= -1;
-      }
-    
-      // Add the speed to the ball
-      x += speedX;
-      y += speedY;
-    }
+function draw() {
+  background(240);
+  fill(ballColor);
+  ellipse(x, y, 70);
+
+  //Defining the walls as the limit for the balls movement, and adding the color change each time the ball hits a wall
+  if (x > width || x < 0) {
+    speedX *= -1;
+    ballColor = random(colors);
+  }
+  if (y > height || y < 0) {
+    speedY *= -1;
+    ballColor = random(colors);
+  }
+  //
+  //Ball-speed
+  x += speedX;
+  y += speedY;
+  //
+}
