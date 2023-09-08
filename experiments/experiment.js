0aa98221ded8e175a7bd0e96227559fda70af32a
vaka21fc <vaka21fc@student.ju.se>
2023-09-08 22:28:11 +0200

    Deleted two files that I had for practice and experimenting, and created a grid with color changing circles for my lab

--- experiments/experiment.js
+++ experiments/experiment.js
@@ -1,8 +1,17 @@
 function setup() {
     createCanvas(500,500);
-    noStroke();
 }
 
 function draw() {
-    background(0);
+    background(255);
+
+    for (let i = 0; i < width; i += 10) {
+        for (let j = 0; j < height; j += 10) {
+            let r = random(255);
+            let g = random(255);
+            let b = random(255);
+          fill(r, g, b);
+          ellipse(i, j, 8, 8);
+        }
+      }
 }
