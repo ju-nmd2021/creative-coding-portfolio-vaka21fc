3e6be3db1f536941159f5f4ccf34a1d1ddc84d90
vaka21fc <vaka21fc@student.ju.se>
2023-09-08 22:32:15 +0200

    Played around with some small attribute-changes

--- experiments/experiment.js
+++ experiments/experiment.js
@@ -1,17 +1,19 @@
 function setup() {
     createCanvas(500,500);
+    noFill();
 }
 
 function draw() {
-    background(255);
+    background(0);
 
-    for (let i = 0; i < width; i += 10) {
-        for (let j = 0; j < height; j += 10) {
+    for (let i = 0; i < width; i += 30) {
+        for (let j = 0; j < height; j += 30) {
             let r = random(255);
             let g = random(255);
             let b = random(255);
-          fill(r, g, b);
-          ellipse(i, j, 8, 8);
+          stroke(r, g, b);
+          strokeWeight(2);
+          rect(i, j, 8, 8);
         }
       }
 }
