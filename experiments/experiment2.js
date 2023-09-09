c6744298e1ba9b1714922f97cf89f085a24bed7c
vaka21fc <vaka21fc@student.ju.se>
2023-09-09 16:04:55 +0200

    Cleaned up code and added a feature so each ball has its own color

--- experiments/experiment2.js
+++ experiments/experiment2.js
@@ -1,8 +1,6 @@
 //Defining my variables
-
 let ballColor;
 let balls = [];
-//
 
 //Defining the color palette for my ball//
 let colors = [
@@ -17,42 +15,20 @@ let colors = [
 //Canvas Setup//
 function setup() {
   createCanvas(500, 500);
-  ballColor = random(colors);
 }
 //
 
 function draw() {
   background(240);
-  balls.forEach(ball => {
+  balls.forEach((ball) => {
     ball.draw();
     ball.update();
   });
-  /*
-  fill(ballColor);
-  ellipse(mouseX, mouseY, random(20, 100));
-
-
-
-  //Defining the walls as the limit for the balls movement, and adding the color change each time the ball hits a wall
-  if (x > width || x < 0) {
-    speedX *= -1;
-    ballColor = random(colors);
-  }
-  if (y > height || y < 0) {
-    speedY *= -1;
-    ballColor = random(colors);
-  }
-  //
-  //Ball-speed
-  x += speedX;
-  y += speedY;
-  //
-  */
 }
 
-function mousePressed(){
-    let ball = new Ball(mouseX, mouseY, random(20,100));
-    balls.push(ball);
+function mousePressed() {
+  let ball = new Ball(mouseX, mouseY, random(20, 100));
+  balls.push(ball);
 }
 
 class Ball {
@@ -60,26 +36,26 @@ class Ball {
     this.x = x;
     this.y = y;
     this.width = width;
-    this.speedX = random(3,12);
-    this.speedY = random(3,12);
-
+    this.speedX = random(-12, 12);
+    this.speedY = random(-12, 12);
+    this.ballColor = random(colors);
   }
   draw() {
-    fill(ballColor);
+    fill(this.ballColor);
     ellipse(this.x, this.y, this.width);
   }
-  update(){
+  update() {
     if (this.x > width || this.x < 0) {
-        this.speedX *= -1;
-        ballColor = random(colors);
-      }
-      if (this.y > height || this.y < 0) {
-        this.speedY *= -1;
-        ballColor = random(colors);
-      }
-      //
-      //Ball-speed
-      this.x += this.speedX;
-      this.y += this.speedY;
+      this.speedX *= -1;
+      this.ballColor = random(colors);
+    }
+    if (this.y > height || this.y < 0) {
+      this.speedY *= -1;
+      this.ballColor = random(colors);
+    }
+    //
+    //Ball-speed
+    this.x += this.speedX;
+    this.y += this.speedY;
   }
 }
