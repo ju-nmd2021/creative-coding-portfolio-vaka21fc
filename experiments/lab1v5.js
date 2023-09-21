c3b7be431ec3cf5f3bb8901860af3232b7eb3351
vaka21fc <vaka21fc@student.ju.se>
2023-09-21 16:51:06 +0200

    changed the shapes to squares

--- experiments/lab1v5.js
+++ experiments/lab1v5.js
@@ -7,12 +7,12 @@ function setup() {
 
 function draw() {
   background(255, 249, 190);
-  rainbowFrame();
+  frame();
   squareParty();
   noLoop();
 }
 
-function rainbowFrame() {
+function frame() {
   let colors = [
     color(188, 100, 123),
     color(221, 171, 67),
@@ -20,51 +20,42 @@ function rainbowFrame() {
     color(90, 62, 47),
     color(208, 97, 46),
   ];
-  let numCircles = 5;
-  let spacing = 100;
+  let numRect = 5;
+  let spacing = 97;
 
-  for (let i = 0; i < numCircles; i += 1) {
-    let startAngle = HALF_PI;
-    let endAngle = -HALF_PI;
+  for (let i = 0; i < numRect; i += 1) {
 
     let fillColor = colors[i];
 
     fill(fillColor);
-    arc(50, 50 + i * spacing, 90, 90, startAngle, endAngle, PIE);
+    rect(10, 10 + i * spacing, 90, 90);
   }
 
-  for (let i = 0; i < numCircles; i += 1) {
+  for (let i = 0; i < numRect; i += 1) {
     push();
-    let startAngle = PI;
-    let endAngle = 0;
+
 
     let fillColor = colors[i];
 
     fill(fillColor);
-    arc(50 + i * spacing, 50, 90, 90, startAngle, endAngle, PIE);
+    rect(10 + i * spacing, 10, 90, 90);
     pop();
   }
 
-  for (let i = 0; i < numCircles; i += 1) {
+  for (let i = 0; i < numRect; i += 1) {
     push();
-    let startAngle = -HALF_PI;
-    let endAngle = HALF_PI;
-
     let fillColor = colors[4 - i];
 
     fill(fillColor);
-    arc(450, 50 + i * spacing, 90, 90, startAngle, endAngle, PIE);
+    rect(398, 10 + i * spacing, 90, 90);
     pop();
   }
-  for (let i = 0; i < numCircles; i += 1) {
+  for (let i = 0; i < numRect; i += 1) {
     push();
-    let startAngle = 0;
-    let endAngle = PI;
-
     let fillColor = colors[4 - i];
 
     fill(fillColor);
-    arc(50 + i * spacing, 450, 90, 90, startAngle, endAngle, PIE);
+    rect(10 + i * spacing, 399, 90, 90);
     pop();
   }
 }
@@ -83,7 +74,7 @@ function squareParty() {
   for (let i = 0; i < 15; i += 1) {
     // The following 2 lines of code was created with the help of Emil Karlsson
     stroke(colors[floor(random(0, 5))]);
-    rect(random(100, 400), random(150, 350), random(20, 50), random(20, 50));
+    rect(random(150, 350), random(150, 350), random(20, 50), random(20, 50));
   }
   pop();
 }
