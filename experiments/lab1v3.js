089b3ea3acee8d38997753aafe053caea2819567
vaka21fc <vaka21fc@student.ju.se>
2023-09-21 15:54:52 +0200

    Updated names, created new files for further experimentation

---
+++ experiments/lab1v3.js
@@ -0,0 +1,94 @@
+//Setup Canvas//
+
+function setup() {
+    createCanvas(500, 500);
+    noStroke();
+  }
+  
+  function draw() {
+    background(246, 239, 214);
+    rainbowFrame();
+    circleParty();
+    noLoop();
+  }
+  
+  function rainbowFrame() {
+    let colors = [
+      color(178, 93, 166),
+      color(102, 136, 195),
+      color(72, 165, 106),
+      color(234, 175, 65),
+      color(206, 74, 74),
+    ];
+    let numCircles = 5;
+    let spacing = 100;
+  
+    for (let i = 0; i < numCircles; i += 1) {
+      let startAngle = HALF_PI;
+      let endAngle = -HALF_PI;
+  
+      let fillColor = colors[i];
+  
+      fill(fillColor);
+      arc(50, 50 + i * spacing, 90, 90, startAngle, endAngle, PIE);
+    }
+  
+    for (let i = 0; i < numCircles; i += 1) {
+      push();
+      let startAngle = PI;
+      let endAngle = 0;
+  
+      let fillColor = colors[i];
+  
+      fill(fillColor);
+      arc(50 + i * spacing, 50, 90, 90, startAngle, endAngle, PIE);
+      pop();
+    }
+  
+    for (let i = 0; i < numCircles; i += 1) {
+      push();
+      let startAngle = -HALF_PI;
+      let endAngle = HALF_PI;
+  
+      let fillColor = colors[4 - i];
+  
+      fill(fillColor);
+      arc(450, 50 + i * spacing, 90, 90, startAngle, endAngle, PIE);
+      pop();
+    }
+    for (let i = 0; i < numCircles; i += 1) {
+      push();
+      let startAngle = 0;
+      let endAngle = PI;
+  
+      let fillColor = colors[4 - i];
+  
+      fill(fillColor);
+      arc(50 + i * spacing, 450, 90, 90, startAngle, endAngle, PIE);
+      pop();
+    }
+  }
+  
+  function circleParty() {
+    let colors = [
+      color(178, 93, 166),
+      color(102, 136, 195),
+      color(72, 165, 106),
+      color(234, 175, 65),
+      color(206, 74, 74),
+    ];
+    push();
+    noFill();
+    strokeWeight(8);
+    for (let i = 0; i < 15; i += 1) {
+      // The following 2 lines of code was created with the help of Emil Karlsson
+      stroke(colors[floor(random(0, 5))]);
+      ellipse(random(100, 400), random(150, 350), random(20, 50), random(20, 50));
+    }
+    pop();
+  }
+  
+  function mousePressed() {
+    loop();
+  }
+  
---
+++ experiments/lab2v3.js
@@ -0,0 +1,88 @@
+//Defining the color palette and notes for the balls//
+let colors;
+let notes;
+let balls;
+let synth;
+//Canvas Setup//
+function setup() {
+  createCanvas(500, 500);
+  colors = [
+    color(112, 214, 255),
+    color(255, 112, 166),
+    color(255, 151, 112),
+    color(255, 214, 112),
+    color(233, 255, 112),
+  ];
+  balls = [];
+  notes = ["C4", "G4", "F4", "A4", "B4"];
+  synth = new Tone.Synth().toDestination();
+  synth.oscillator.type = 'sawtooth';
+}
+
+function draw() {
+  background(240);
+  balls.forEach((ball) => {
+    ball.draw();
+    ball.update();
+  });
+}
+
+//Generating new balls when the mouse is pressed//
+function mousePressed() {
+  let ball = new Ball(mouseX, mouseY, random(20, 100));
+  balls.push(ball);
+}
+
+function ballSound(ballColor) {
+  if (ballColor == colors[0]) {
+    synth.triggerAttackRelease(notes[0], "4n");
+  }
+  if (ballColor == colors[1]) {
+    synth.triggerAttackRelease(notes[1], "4n");
+  }
+  if (ballColor == colors[2]) {
+    synth.triggerAttackRelease(notes[2], "4n");
+  }
+  if (ballColor == colors[3]) {
+    synth.triggerAttackRelease(notes[3], "4n");
+  }
+  if (ballColor == colors[4]) {
+    synth.triggerAttackRelease(notes[4], "4n");
+  }
+}
+
+class Ball {
+  constructor(x, y, width) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.speedX = random(-12, 12);
+    this.speedY = random(-12, 12);
+    if (this.speedX == 0) {
+      this.speedX += 3;
+    }
+    if (this.speedY == 0) {
+      this.speedY += 3;
+    }
+    this.ballColor = random(colors);
+    ballSound(this.ballColor);
+  }
+  draw() {
+    fill(this.ballColor);
+    ellipse(this.x, this.y, this.width);
+  }
+  update() {
+    if (this.x > width || this.x < 0) {
+      this.speedX *= -1;
+      this.ballColor = random(colors);
+    }
+    if (this.y > height || this.y < 0) {
+      this.speedY *= -1;
+      this.ballColor = random(colors);
+    }
+
+    //Ball-speed
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+}
