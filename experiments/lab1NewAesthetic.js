aad99cc89a2f0fd6a297d37476be28f259f5eba6
vaka21fc <vaka21fc@student.ju.se>
2023-09-21 14:29:06 +0200

    I removed the randomly generating circles in the middle of the frame. Then I rearranged my half-circles into a different pattern. To incorporate some randomness, the colors will now change with every mouse click.

--- experiments/lab1NewAesthetic.js
+++ experiments/lab1NewAesthetic.js
@@ -1,14 +1,14 @@
 //Setup Canvas//
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(500, 445);
     noStroke();
   }
   
   function draw() {
     background(246, 239, 214);
     rainbowFrame();
-    circleParty();
+    rectangleParty();
     noLoop();
   }
   
@@ -20,29 +20,18 @@ function setup() {
       color(234, 175, 65),
       color(206, 74, 74),
     ];
-    let numCircles = 5;
-    let spacing = 100;
+    let numCircles = 3;
+    let spacing = 145;
   
+for   (let p=0; p<7; p+=1) { 
     for (let i = 0; i < numCircles; i += 1) {
       let startAngle = HALF_PI;
       let endAngle = -HALF_PI;
   
-      let fillColor = colors[i];
+      let fillColor = (colors[floor(random(0, 5))]);
   
       fill(fillColor);
-      arc(50, 50 + i * spacing, 90, 90, startAngle, endAngle, PIE);
-    }
-  
-    for (let i = 0; i < numCircles; i += 1) {
-      push();
-      let startAngle = PI;
-      let endAngle = 0;
-  
-      let fillColor = colors[i];
-  
-      fill(fillColor);
-      arc(50 + i * spacing, 50, 90, 90, startAngle, endAngle, PIE);
-      pop();
+      arc(2 * p * 35 + 40, 40 + i * spacing, 70, 70, startAngle, endAngle, PIE);
     }
   
     for (let i = 0; i < numCircles; i += 1) {
@@ -50,26 +39,17 @@ function setup() {
       let startAngle = -HALF_PI;
       let endAngle = HALF_PI;
   
-      let fillColor = colors[4 - i];
+      let fillColor = (colors[floor(random(0, 5))]);
   
       fill(fillColor);
-      arc(450, 50 + i * spacing, 90, 90, startAngle, endAngle, PIE);
+      arc(2 * p * 35 + 40, 112.5 + i * spacing, 70, 70, startAngle, endAngle, PIE);
       pop();
     }
-    for (let i = 0; i < numCircles; i += 1) {
-      push();
-      let startAngle = 0;
-      let endAngle = PI;
-  
-      let fillColor = colors[4 - i];
-  
-      fill(fillColor);
-      arc(50 + i * spacing, 450, 90, 90, startAngle, endAngle, PIE);
-      pop();
-    }
+}
   }
+
   
-  function circleParty() {
+  function rectangleParty() {
     let colors = [
       color(178, 93, 166),
       color(102, 136, 195),
@@ -78,13 +58,13 @@ function setup() {
       color(206, 74, 74),
     ];
     push();
-    noFill();
-    strokeWeight(8);
-    for (let i = 0; i < 15; i += 1) {
-      // The following 2 lines of code was created with the help of Emil Karlsson
-      stroke(colors[floor(random(0, 5))]);
-      ellipse(random(100, 400), random(150, 350), random(20, 50), random(20, 50));
-    }
+    // noFill();
+    // strokeWeight(8);
+    // for (let i = 0; i < 15; i += 1) {
+    //   // The following 2 lines of code was created with the help of Emil Karlsson
+    //   stroke(colors[floor(random(0, 5))]);
+    //   rect(random(100, 400), random(150, 350), random(20, 50), random(20, 50));
+    // }
     pop();
   }
   
